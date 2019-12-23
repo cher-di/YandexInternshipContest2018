84afe7f13c9a762181f46772474b97d61f7928c6
Dmitry <cher-di@mail.ru>
2019-12-23 16:47:12 +0300

    Changed solution for task#4 to recursive solutions

--- 4.py
+++ 4.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, choice
 
 
 def find_strangers(team: set, left_players: set, edges: frozenset) -> set:
@@ -17,17 +17,43 @@ def will_be_fully_connected(new_vertex: int, vertexes: set, edges: frozenset) ->
     return True
 
 
-if __name__ == '__main__':
-    n, m = (int(i) for i in input().split())
-    edges = set()
-    for i in range(m):
-        u, v = (int(i) for i in input().split())
-        edges.add(frozenset((u, v)))
-    edges = frozenset(edges)
+def merge_commands(team_a1: set, team_b1: set, team_a2: set, team_b2: set, edges: frozenset):
+    def can_be_merged(team1, team2):
+        for player in team2:
+            if not will_be_fully_connected(player, team1, edges):
+                return False
+        return True
 
-    team_a = {randint(1, n)}
+    a2_available_commands = set()
+    b2_available_commands = set()
+    if can_be_merged(team_a1, team_a2):
+        a2_available_commands.add(team_a1)
+    if can_be_merged(team_b1, team_a2):
+        a2_available_commands.add(team_b1)
+    if can_be_merged(team_a1, team_b2):
+        b2_available_commands.add(team_a1)
+    if can_be_merged(team_b1, team_b2):
+        b2_available_commands.add(team_b1)
+
+    if len(a2_available_commands) <= len(b2_available_commands):
+        chosen_command = choice(tuple(a2_available_commands))
+        chosen_command.union(team_a2)
+        left_command = team_a1 if team_a1 != chosen_command else team_b1
+        left_command.union(team_b2)
+    else:
+        chosen_command = choice(tuple(b2_available_commands))
+        chosen_command.union(team_b2)
+        left_command = team_a1 if team_a1 != chosen_command else team_b1
+        left_command.union(team_a2)
+
+
+def make_commands(people: set, edges: frozenset, good_teams_result=True) -> tuple:
+    if not good_teams_result:
+        return None, None, False
+
+    team_a = {choice(tuple(people))}
     team_b = set()
-    left_players = set(range(1, n + 1)) - team_a
+    left_players = people - team_a
     good_teams = True
     while good_teams:
         strangers = find_strangers(team_a, left_players, edges)
@@ -55,17 +81,26 @@ if __name__ == '__main__':
             break
 
     if not good_teams:
+        return None, None, False
+    else:
+        if left_players:
+            team_a2, team_b2, result = make_commands(left_players, edges, good_teams)
+            merge_commands(team_a, team_b, team_a2, team_b2, edges)
+        return team_a, team_b, True
+
+
+if __name__ == '__main__':
+    n, m = (int(i) for i in input().split())
+    edges = set()
+    for i in range(m):
+        u, v = (int(i) for i in input().split())
+        edges.add(frozenset((u, v)))
+    edges = frozenset(edges)
+
+    team_a, team_b, result = make_commands(set(range(1, n + 1)), edges)
+    if not result:
         print(-1)
     else:
-        for player in left_players:
-            if will_be_fully_connected(player, team_a, edges):
-                team_a.add(player)
-            elif will_be_fully_connected(player, team_b, edges):
-                team_b.add(player)
-            else:
-                print(-1)
-                break
-        else:
-            print(len(team_a))
-            print(" ".join(str(player) for player in team_a))
-            print(" ".join(str(player) for player in team_b))
+        print(len(team_a))
+        print(" ".join(str(player) for player in team_a))
+        print(" ".join(str(player) for player in team_b))
